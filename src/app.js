271a3afdb78c79e34691f6a4d26163cd073171ef
Mike Ward <ward.michael51@gmail.com>
2016-05-11 21:27:27 -0400

    stable heroku version

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 (function() {
     'use strict';
-    
-    var app = angular.module('app', []);
+    angular.module('app', []);
 }());
--- src/mainCtrl.js
+++ src/mainCtrl.js
@@ -1,7 +1,10 @@
 (function(){
     'use strict';
 
-    angular.module("app").controller("mainCtrl", function($scope){
+    angular
+        .module("app")
+        .controller("mainCtrl", ["$scope", function($scope) {
         $scope.name = "Test App"
-    });
+    }]);
+    
 }());
