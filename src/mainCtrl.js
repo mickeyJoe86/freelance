f10b23b5a51b394032232c0765461c6415a0f6a6
Mike Ward <ward.michael51@gmail.com>
2016-07-31 15:10:35 -0400

    fixed success validation message

--- src/mainCtrl.js
+++ src/mainCtrl.js
@@ -6,11 +6,13 @@
         .controller("mainCtrl", ["$scope", "$http", function($scope, $http) {
         $scope.name = "Test App";
         $scope.cf = {};
+        $scope.cf.successMessage = false
+        $scope.cf.failureMessage = false;
         
         $scope.submitForm = function() {
             if($scope.cf.name && $scope.cf.email && $scope.cf.email && $scope.cf.formBody) {
+                $scope.cf.successMessage = true;
                 $http.post('/contact', $scope.cf).then(function(){
-                    $scope.cf.successMessage = true;
                     $scope.cf = {};
                 }, function() {
                     $scope.cf.failureMessage = true;
