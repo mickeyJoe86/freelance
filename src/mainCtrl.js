9a2c43539199f94086b2e2bfe2f290f5ed27c808
Mike Ward <ward.michael51@gmail.com>
2016-08-01 21:43:47 -0400

    added validation to separate object

--- src/mainCtrl.js
+++ src/mainCtrl.js
@@ -6,18 +6,19 @@
         .controller("mainCtrl", ["$scope", "$http", function($scope, $http) {
         $scope.name = "Test App";
         $scope.cf = {};
-        $scope.cf.successMessage = false
-        $scope.cf.failureMessage = false;
+        $scope.validation = {
+            successMessage: false,
+            failureMessage: false
+        };
         
         $scope.submitForm = function() {
             if($scope.cf.name && $scope.cf.email && $scope.cf.email && $scope.cf.formBody) {
-                
                 $http.post('/contact', $scope.cf).then(function(){
                     $scope.cf = {};
-                    $scope.cf.successMessage = true;
+                    $scope.validation.successMessage = true;
                 }, function() {
                     $scope.cf = {};
-                    $scope.cf.failureMessage = true;
+                    $scope.validation.failureMessage = true;
                 });
             } else {
                 //Add validation messages
